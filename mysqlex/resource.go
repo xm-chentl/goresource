bb81ac89c3d510afed6ba210c179a48c67aef631
ctl <359343287@qq.com>
2023-04-12 09:34:48 +0800

    feat: add connection pool config

--- mysqlex/resource.go
+++ mysqlex/resource.go
@@ -14,6 +14,11 @@ type resource struct {
 	db  *gorm.DB
 }
 
+type Config struct {
+	MaxIdleConns int // 最大空闲连接
+	MaxOpenConns int // 使用最大连接数
+}
+
 // Db 参数请按 ctx uow
 func (f *resource) Db(args ...interface{}) goresource.IRepository {
 	repo := &repository{}
@@ -38,7 +43,7 @@ func (f *resource) Uow() goresource.IUnitOfWork {
 	return newUnitOfWork(f.db)
 }
 
-func New(dsn string) goresource.IResource {
+func New(dsn string, configs ...Config) goresource.IResource {
 	if dsn == "" {
 		panic("mysqlex.New parameter dsn is empty")
 	}
@@ -47,6 +52,21 @@ func New(dsn string) goresource.IResource {
 	if err != nil {
 		panic("mysqlex.New open database is failed err: " + err.Error())
 	}
+
+	// add connection pool mode
+	sqlDb, err := db.DB()
+	if err != nil {
+		panic("open db failed: " + err.Error())
+	}
+	if len(configs) > 0 {
+		cfg := configs[0]
+		sqlDb.SetMaxIdleConns(cfg.MaxIdleConns)
+		sqlDb.SetMaxOpenConns(cfg.MaxOpenConns)
+	} else {
+		sqlDb.SetMaxIdleConns(10)
+		sqlDb.SetMaxOpenConns(100)
+	}
+
 	return &resource{
 		dsn: dsn,
 		db:  db,
